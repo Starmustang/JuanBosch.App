c3100207ab9b6286cada0b8dcd5fbe99f644e4d2
Wilson Bienvenido Otaño Mateo <102930523+Starmustang@users.noreply.github.com>
2025-08-02 20:14:54 -0400

    Add DateMedic EF configuration and update model
    Introduced DateMedicConfiguration for Entity Framework mapping and constraints. Updated DateMedic model to use 'required' for navigation properties and HospitalMedicDate, improving null safety and data integrity.

---
+++ Data/Configurations/dates/DateMedicConfiguration.cs
@@ -0,0 +1,44 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using JuanBosch.App.Models.Dates;
+
+namespace JuanBosch.App.Data.Configurations.dates
+{
+    public class DateMedicConfiguration : IEntityTypeConfiguration<DateMedic>
+    {
+        public void Configure(EntityTypeBuilder<DateMedic> builder)
+        {
+            builder.ToTable("DateMedics");
+            builder.HasKey(d => d.DateMedicId);
+            builder.Property(d => d.DateMedicId)
+            .ValueGeneratedOnAdd();
+
+            builder.Property(d => d.DateMedicDate)
+            .IsRequired();
+
+            builder.Property(d => d.HospitalMedicDate)
+            .IsRequired()
+            .HasMaxLength(25);
+
+            builder.Property(d => d.ConsultationTypeId)
+            .IsRequired();
+
+            builder.HasOne(d => d.Patient)
+            .WithMany(p => p.DateMedics)
+            .HasForeignKey(d => d.PatientId);
+            
+
+            builder.HasOne(d => d.Doctor)
+            .WithMany(p => p.DateMedics)
+            .HasForeignKey(d => d.DoctorId);
+
+            builder.HasOne(d => d.ConsultationType)
+            .WithMany(p => p.DateMedics)
+            .HasForeignKey(d => d.ConsultationTypeId);
+
+            builder.HasOne(d => d.DateDoctor)
+            .WithMany(p => p.DateMedics)
+            .HasForeignKey(d => d.DateDoctorId);
+        }
+    }
+}
--- Models/Dates/DateMedic.cs
+++ Models/Dates/DateMedic.cs
@@ -7,15 +7,15 @@ namespace JuanBosch.App.Models.Dates
     public class DateMedic
     {
         public int DateMedicId { get; set; }
-        public Patient Patient { get; set; }
+        public required Patient Patient { get; set; }
         public int PatientId { get; set; }
-        public DoctorMedic Doctor { get; set; }
+        public required DoctorMedic Doctor { get; set; }
         public int DoctorId { get; set; }
         public DateTime DateMedicDate { get; set; }
-        public string HospitalMedicDate { get; set; }
+        public required string HospitalMedicDate { get; set; }
         public int ConsultationTypeId { get; set; }
-        public ConsultationType ConsultationType { get; set; }
-        public DateDoctor DateDoctor { get; set; }
+        public required ConsultationType ConsultationType { get; set; }
+        public required DateDoctor DateDoctor { get; set; }
         public int DateDoctorId { get; set; }
 
        
